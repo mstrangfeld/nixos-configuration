afe42de31efe21f22cfe8a35841cfac197371b7c
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-07 16:36:20 +0200

    add audio to Kronos

--- hosts/Kronos.nix
+++ hosts/Kronos.nix
@@ -18,6 +18,16 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome3.enable = true;
 
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
   fileSystems."/" = { device = "/dev/disk/by-label/nixos"; };
   fileSystems."/boot" = { device = "/dev/disk/by-label/boot"; };
   swapDevices = [
