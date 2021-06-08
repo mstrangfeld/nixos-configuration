3d904c5b0e650a7648ba3eefa38eb5ed458680d1
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-09 01:37:28 +0200

    add kubectx package

--- users/profiles/development/virtualisation/default.nix
+++ users/profiles/development/virtualisation/default.nix
@@ -5,6 +5,7 @@
     dive # A tool for exploring each layer in a docker image
     k9s # Kubernetes CLI To Manage Your Clusters In Style
     kind # Kubernetes IN Docker
+    kubectx # Fast way to switch between clusters and namespaces in kubectl!
     kubernetes-helm # A package manager for Kubernetes
     lazydocker # A simple terminal UI for both docker and docker-compose
     lens # The Kubernetes IDE
