04bcd65c9f18fef85ad5e99a591d29f89c70a775
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-10 12:29:05 +0200

    add spotify-4k deviceScaleFactor

--- users/profiles/entertainment/default.nix
+++ users/profiles/entertainment/default.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
+let
+  spotify-4k = pkgs.spotify.override { deviceScaleFactor = 1.5; };
+in
 {
   home.packages = with pkgs; [
     minecraft # Official launcher for Minecraft, a sandbox-building game
     vlc # Cross-platform media player and streaming server
-    spotify # Play music from the Spotify music service
+    spotify-4k # Play music from the Spotify music service
     spotify-tui # Spotify for the terminal written in Rust
     steam # A digital distribution platform
   ];
