ba2e9cdcca952c3324815fa8d09b7141c126b877
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-15 20:23:35 +0200

    add java build tools

--- profiles/development/java/default.nix
+++ profiles/development/java/default.nix
@@ -3,10 +3,13 @@
   environment = {
     systemPackages = with pkgs; [
       # android-studio # The Official IDE for Android
+      ant # A Java-based build tool
       gradle # Enterprise-grade build system
+      groovy # An agile dynamic language for the Java Platform
       jdk # The open-source Java Development Kit
       kotlin # General purpose programming language
       ktlint # An anti-bikeshedding Kotlin linter with built-in formatter
+      maven # Build automation tool (used primarily for Java projects)
     ];
   };
 }
