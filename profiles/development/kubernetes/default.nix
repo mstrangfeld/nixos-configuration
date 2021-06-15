6c624937017fbaf9653f5a7c7cbf6c73c15e168e
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-15 21:13:51 +0200

    add kubectl to kubernetes development profile

--- profiles/development/kubernetes/default.nix
+++ profiles/development/kubernetes/default.nix
@@ -5,6 +5,7 @@
     systemPackages = with pkgs; [
       k9s # Kubernetes CLI To Manage Your Clusters In Style
       kind # Kubernetes IN Docker
+      kubectl # Kubernetes CLI
       kubectx # Fast way to switch between clusters and namespaces in kubectl!
       kubernetes-helm # A package manager for Kubernetes
       lens # The Kubernetes IDE
