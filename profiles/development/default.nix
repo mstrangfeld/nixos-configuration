f163ad6e7490a555b5a7829b70a7acd470f15f74
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-15 20:26:11 +0200

    import new development profiles into standard development profile

--- profiles/development/default.nix
+++ profiles/development/default.nix
@@ -1,8 +1,31 @@
-{ ... }:
+{ pkgs
+, headless ? false
+, ...
+}:
 {
   imports = [
+    ./arduino
     ./database
+    ./go
+    ./haskell
     ./java
-    ./k8s
+    ./julia
+    ./kubernetes
+    ./markup
+    ./python
+    ./rust
   ];
+
+  environment = {
+    systemPackages = with pkgs; [
+      bash # GNU Bourne-Again Shell, the de facto standard shell on Linux
+      clang # A C language family frontend for LLVM (wrapper script)
+      clang-tools # Standalone command line tools for C++ development
+      gdb # The GNU Project debugger
+      gnumake # A tool to control the generation of non-source files from sources
+      gcc # GNU Compiler Collection, version 10.3.0 (wrapper script)
+      perl # The standard implementation of the Perl 5 programmming language
+      rr # Records nondeterministic executions and debugs them deterministically
+    ];
+  };
 }
