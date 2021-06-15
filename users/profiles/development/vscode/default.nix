2a1b17047895ce44520dfad326d399ed05e14859
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-15 20:25:48 +0200

    add vscode extensions

--- users/profiles/development/vscode/default.nix
+++ users/profiles/development/vscode/default.nix
@@ -4,6 +4,16 @@
     enable = true;
     extensions = with pkgs; [
       vscode-extensions.bbenoist.Nix
+      vscode-extensions.vscodevim.vim
+      vscode-extensions.haskell.haskell
+      vscode-extensions.golang.Go
+      vscode-extensions.editorconfig.editorconfig
+      vscode-extensions.redhat.vscode-yaml
+      vscode-extensions.ms-python.python
+      vscode-extensions.ms-python.vscode-pylance
+      vscode-extensions.pkief.material-icon-theme
+      vscode-extensions.james-yu.latex-workshop
+      vscode-extensions.ms-kubernetes-tools.vscode-kubernetes-tools
     ];
     haskell = {
       enable = false;
