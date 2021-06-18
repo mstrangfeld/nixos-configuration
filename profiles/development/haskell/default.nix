2e095f3393a9b41002b64ccfd125d7a0d29d4788
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-18 18:22:23 +0200

    add stack to haskell profile

--- profiles/development/haskell/default.nix
+++ profiles/development/haskell/default.nix
@@ -4,6 +4,7 @@
     systemPackages = with pkgs; [
       ghc # The Glasgow Haskell Compiler
       haskell-language-server # LSP server for GHC
+      stack # The Haskell Tool Stack
     ];
   };
 }
