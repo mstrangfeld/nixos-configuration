78dfcd0a6baf99a5ca585b91739c975f86267b3b
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-18 21:00:52 +0200

    add extra xsession init

--- users/profiles/x/xmonad/default.nix
+++ users/profiles/x/xmonad/default.nix
@@ -3,12 +3,26 @@
   xsession = {
     enable = true;
 
+    initExtra = ''
+      # Disable screen blank
+      ${pkgs.util-linux}/bin/setterm --blank 0 --powersave off
+      # Disable screen saver blanking
+      ${pkgs.xorg.xset}/bin/xset s off
+      # Restore the wallpaper
+      ${pkgs.nitrogen}/bin/nitrogen --restore &
+      # PulseAudio system tray
+      ${pkgs.pasystray}/bin/pasystray &
+      # NetworkManager Applet
+      ${pkgs.gnome3.networkmanagerapplet}/bin/nm-applet --sm-disable --indicator &
+    '';
+
     windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
       extraPackages = hp: [
         hp.dbus
         hp.monad-logger
+        hp.xmonad-contrib
       ];
       config = ./config.hs;
     };
