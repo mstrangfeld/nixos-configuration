583e79edb0bbff0fd98736df2b1ec083fa459baf
Marvin Strangfeld <marvin@strangfeld.io>
2021-06-19 21:00:41 +0200

    fix rofi font

--- users/profiles/x/services/rofi/default.nix
+++ users/profiles/x/services/rofi/default.nix
@@ -4,7 +4,7 @@
   programs.rofi = {
     enable = true;
     terminal = "${pkgs.alacritty}/bin/alacritty";
-    font = "Fira Code Nerd Font";
+    font = "FiraCode Nerd Font 16";
     location = "center";
     padding = 10;
     lines = 10;
