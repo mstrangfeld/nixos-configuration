601a4c81b2036d2baa07a097cf205511be3720b2
Marvin Strangfeld <marvin@strangfeld.io>
2021-08-12 19:36:07 +0200

    fix update

--- users/profiles/development/vscode/default.nix
+++ users/profiles/development/vscode/default.nix
@@ -9,14 +9,14 @@
       ms-vscode.cpptools
       ms-azuretools.vscode-docker
       editorconfig.editorconfig
-      golang.Go
+      golang.go
       haskell.haskell
       justusadam.language-haskell
       ms-toolsai.jupyter
       ms-kubernetes-tools.vscode-kubernetes-tools
       james-yu.latex-workshop
       pkief.material-icon-theme
-      bbenoist.Nix
+      bbenoist.nix
       ms-python.vscode-pylance
       ms-python.python
       # jprestidge.theme-material-theme # Not yet implemented
