0ed0a700dcb4791e0ebdf89242b8e609134539cc
Marvin Strangfeld <marvin@strangfeld.io>
2021-09-03 12:31:53 +0200

    add gopro-webcam package

---
+++ pkgs/tools/video/gopro-webcam/default.nix
@@ -0,0 +1,31 @@
+{ lib, stdenv, fetchFromGitHub, makeWrapper, vlc, ffmpeg, curl, iproute2, ... }:
+stdenv.mkDerivation {
+  pname = "gopro-webcam";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "jschmid1";
+    repo = "gopro_as_webcam_on_linux";
+    rev = "master";
+    sha256 = "sha256-/Z0usE+c9RFa/QjCA9+GE8xsGynKo5d9uaSkClWXHw8=";
+  };
+
+  buildInputs = [ makeWrapper ];
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -m755 gopro $out/bin/gopro
+    wrapProgram $out/bin/gopro --prefix PATH : ${lib.makeBinPath [ vlc ffmpeg curl iproute2 ]}
+  '';
+
+  meta = {
+    description = "Allows to use your GoPro camera as a webcam on linux";
+    homepage = "https://github.com/jschmid1/gopro_as_webcam_on_linux";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ mstrangfeld ];
+    platforms = lib.platforms.unix;
+  };
+}
