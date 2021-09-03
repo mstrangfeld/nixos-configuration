2f6e48b675aa84f722db655a892b47a210f79adf
Marvin Strangfeld <marvin@strangfeld.io>
2021-09-03 12:31:08 +0200

    update system

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 final: prev: {
   ttyper = prev.callPackage ./games/ttyper { };
   rocketchat-desktop = prev.callPackage ./applications/networking/instant-messengers/rocketchat-desktop { };
+  gopro-webcam = prev.callPackage ./tools/video/gopro-webcam { };
 }
