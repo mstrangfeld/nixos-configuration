eb80f41fcdaba8ac551e1b9f05fab28e8b66961e
Marvin Strangfeld <marvin@strangfeld.io>
2021-10-18 10:11:15 +0200

    use the steam module

--- profiles/entertainment/default.nix
+++ profiles/entertainment/default.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 {
+
+  programs.steam.enable = true;
+
   environment = {
     systemPackages = with pkgs; [
       minecraft # Official launcher for Minecraft, a sandbox-building game
-      steam # A digital distribution platform
       ttyper # Terminal-based typing test
+      xboxdrv # Xbox/Xbox360 (and more) gamepad driver for Linux that works in userspace
     ];
   };
 }
